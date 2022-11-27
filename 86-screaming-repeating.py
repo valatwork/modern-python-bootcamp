75acb9e46493500cc13ed4fceabc9b3c5a5661aa
valatwork <val.giardini@bcdtravel.nl>
2022-11-27 23:26:59 +0100

    Empty commit

---
+++ 86-screaming-repeating.py
@@ -0,0 +1,10 @@
+times = input("How many times do I have to tell you? ")
+times = int(times)
+
+# Simplest Version
+for time in range(times):
+	print("CLEAN UP YOUR ROOM")
+
+# Version 2
+# for time in range(times):
+# 	print(f"time {time+1}:CLEAN UP YOUR ROOM")
