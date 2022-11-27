2e36c15080f392a8f1ff0f7a706e76f2c4c66a35
valatwork <val.giardini@bcdtravel.nl>
2022-11-27 23:01:14 +0100

    My empty commit with a message

---
+++ 83-for-loops-basics.py
@@ -0,0 +1,17 @@
+# for item in iterable_object:
+
+# an iterable object is a collection of data
+
+# item references the current position of our iterator within the iterable
+# it will iterate (run through) every item of the collection 
+# and then go away when it has vitised all items
+
+# for number in range(1,8):
+#     print(number)
+
+# for x in range (1,10):
+#     print(x)
+#     print(x*x)
+
+for letter in "coffee":
+    print(letter*10)
---
+++ 84-exploring-ranges.py
@@ -0,0 +1,7 @@
+range(7) # integers from 0 to 7
+
+range(1,8) # integers from 1 to 7
+
+range(1,10,2) # odds from 1 to 10
+
+range(7,0,-1) # integers from 7 to 1
