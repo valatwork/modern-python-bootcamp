cef0fbea5945d1f3065f473647782485fea26d7b
Val Giardini <105278308+valatwork@users.noreply.github.com>
2023-02-24 17:22:18 +0100

    Update 137-dict-exercise-lists.py

--- 137-dict-exercise-lists.py
+++ 137-dict-exercise-lists.py
@@ -3,6 +3,10 @@ person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
 # use the person variable in your answer
 answer = {x[0]:x[1] for x in person} # my solution
 
+# unpacking
+
 answer = {k:v for k,v in person}
+
+# mapping
 
 answer = dict(person)
