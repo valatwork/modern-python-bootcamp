b0ad286fc52c1c97282ee3e366f4c60986d58446
Val Giardini <105278308+valatwork@users.noreply.github.com>
2023-03-04 11:22:06 +0100

    Update 208-extract-full-name-exercise.py

--- 208-extract-full-name-exercise.py
+++ 208-extract-full-name-exercise.py
@@ -1,5 +1,7 @@
-# Write a function called extract_full_name. 
-# This function should accept a list of dictionaries and return a new list of strings with the first and last name keys in each dictionary concatenated.
+'''
+Write a function called extract_full_name. 
+This function should accept a list of dictionaries and return a new list of strings with the first and last name keys in each dictionary concatenated.
+'''
 
 '''
 names = [{'first': 'Elie', 'last': 'Schoppik'}, {'first': 'Colt', 'last': 'Steele'}]
