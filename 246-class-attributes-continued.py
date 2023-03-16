e66a3db76df04dedb0cb27b150dc40c7a0994e9a
Val Giardini <105278308+valatwork@users.noreply.github.com>
2023-03-16 13:52:57 +0100

    Update and rename 246-class-attributes-continues.py to 246-class-attributes-continued.py

--- 246-class-attributes-continued.py
+++ 246-class-attributes-continued.py
@@ -16,5 +16,16 @@ class Pet:
 cat = Pet("Blue", "cat")
 dog = Pet("Wyatt", "dog")
 
+## exercise
 
-
+class Chicken:
+    total_eggs = 0
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.eggs = 0
+    
+    def lay_egg(self):
+        self.eggs += 1
+        Chicken.total_eggs += 1
+        return self.eggs
